12fb47b341177f0522ec630e2a4ee0a590ee18a8
TheKingmaker2 <119481512+TheKingmaker2@users.noreply.github.com>
2024-07-03 11:41:55 +0200

    Add files via upload
    Added playRound function.  It gets human and computer choice as arguments and prints the winner on the screen and also updates humanScore and computerScore variables.

--- script.js
+++ script.js
@@ -22,3 +22,48 @@ function getHumanChoice () {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound (humanChoice, computerChoice) {
+    humanChoice = getHumanChoice().toLowerCase();
+    computerChoice = getComputerChoice();
+
+    if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("You win! Rock beats Paper");
+        humanScore++;
+    }
+
+    else if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You lose! Paper beats Rock");
+        computerScore++;
+    }
+
+    else if (humanChoice == "rock" && computerChoice == "rock")
+        console.log("It's a draw! Both choices are Rock");
+
+    else if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You win! Scissors beat Paper");
+        humanScore++;
+    }
+
+    else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You lose! Rock beats Scissors");
+        computerScore++;
+    }
+
+    else if (humanChoice == "scissors" && computerChoice == "scissors")
+        console.log("It's a draw! Both choices are Scissors");
+
+    else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You win! Paper beats Rock");
+        humanScore++;
+    }
+
+    else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("You lose! Scissors beat Paper");
+        computerScore++;
+    }
+
+    else if (humanChoice == "paper" && computerChoice == "paper")
+        console.log("It's a draw! Both choices are Paper");
+}
+
